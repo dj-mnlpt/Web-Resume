b14ed8df601ebf8f6a39eb566c869eb773842373
darwin.xpanse@gmail.com <darwin_xpanse@gmail.com>
2020-03-20 15:58:23 +0800

    3:58 PM commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  theme: {
+    extend: {
+      height: {
+        '72':'18rem',
+        '80':'20rem',
+        '88':'22rem',
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
